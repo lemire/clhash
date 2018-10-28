fab8df827f8f98a99e274d6459f0060d1d10aad3
Hung Dang <hungptit@gmail.com>
2018-10-28 01:31:19 -0400

    Add a python script which automatically read and plot the benchmark results.

---
+++ benchmark/plot_benchmark_results.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import argparse
+import matplotlib.pyplot as plt
+import json
+import numpy as np
+
+# Get the maximum length of the test string.
+parser = argparse.ArgumentParser(description='Required input arguments.')
+parser.add_argument('prefix', metavar='prefix', type=str,
+                    help='Prefix of JSON data files')
+parser.add_argument('maximum_length', metavar='maximum_length', type=int,
+                    help='The maximum length of tested strings')
+
+args = parser.parse_args()
+prefix = args.prefix
+N = args.maximum_length
+print("Prefix: ", prefix)
+print("Maximum string length: ", N)
+N = args.maximum_length
+
+# Get the benchmark results for each hash functions.
+std_hash_results = np.zeros(N, dtype=float)
+clhash_results = np.zeros(N, dtype=float)
+boost_hash_results = np.zeros(N, dtype=float)
+for idx in range(1, N):
+    data_file = prefix + str(idx) + ".json"
+    # print("data_file: ", data_file)
+    data = json.load(open(data_file))
+    benchmark_results = data["benchmarks"]
+    # print(benchmark_results)
+
+    for info in benchmark_results:
+        benchmark_name = info["name"]
+        if (benchmark_name == "std_hash_string"):
+            std_hash_results[idx] = info["real_time"]
+        elif (benchmark_name == "clhash_string"):
+            clhash_results[idx] = info["real_time"]
+        elif (benchmark_name == "boost_hash_string"):
+            boost_hash_results[idx] = info["real_time"]
+        else:
+            print("How can we get here?")
+
+# Plot results
+x = np.arange(1, N + 1, 1)
+line1, line2, line3 = plt.plot(
+    x, std_hash_results, "r", x, clhash_results, "b", x, boost_hash_results, "o")
+plt.xlabel("String length (bytes)")
+plt.ylabel("Run time (ns)")
+# plt.legend("std::hash", "clhash", "boost::hash")
+plt.ylim(0, 200)
+plt.xlim(1, N)
+plt.grid()
+plt.legend((line1, line2, line3), ('std::hash', 'clhash', 'boost::hash'))
+plt.show()
