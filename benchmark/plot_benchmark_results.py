c42ec16beec0bdc4ca18d7f018ff236bc3d96fa6
Hung Dang <hdang@athenahealth.com>
2018-11-08 14:15:29 -0500

    Cleanup plot script

--- benchmark/plot_benchmark_results.py
+++ benchmark/plot_benchmark_results.py
@@ -4,7 +4,44 @@ import matplotlib.pyplot as plt
 import json
 import numpy as np
 
-# Get the maximum length of the test string.
+def plot_results(prefix, N):
+    # Get the benchmark results for each hash functions.
+    std_hash_results = np.zeros(N, dtype=float)
+    clhash_results = np.zeros(N, dtype=float)
+    boost_hash_results = np.zeros(N, dtype=float)
+    for idx in range(1, N):
+        data_file = prefix + str(idx) + ".json"
+        # print("data_file: ", data_file)
+        data = json.load(open(data_file))
+        benchmark_results = data["benchmarks"]
+        # print(benchmark_results)
+    
+    for info in benchmark_results:
+        benchmark_name = info["name"]
+        if (benchmark_name == "std_hash_string"):
+            std_hash_results[idx] = info["real_time"]
+        elif (benchmark_name == "clhash_string"):
+            clhash_results[idx] = info["real_time"]
+        elif (benchmark_name == "boost_hash_string"):
+            boost_hash_results[idx] = info["real_time"]
+        else:
+            print("How can we get here?")
+
+            
+    # Plot results
+    x = np.arange(1, N + 1, 1)
+    line1, line2, line3 = plt.plot(
+        x, std_hash_results, "r", x, clhash_results, "b", x, boost_hash_results, "o")
+    plt.xlabel("String length (bytes)")
+    plt.ylabel("Run time (ns)")
+    plt.ylim(0, 200)
+    plt.xlim(1, N)
+    plt.grid()
+    plt.legend((line1, line2, line3), ('std::hash', 'clhash', 'boost::hash'))
+    plt.show()
+
+    
+# Parse input arguments
 parser = argparse.ArgumentParser(description='Required input arguments.')
 parser.add_argument('prefix', metavar='prefix', type=str,
                     help='Prefix of JSON data files')
@@ -18,37 +55,5 @@ print("Prefix: ", prefix)
 print("Maximum string length: ", N)
 N = args.maximum_length
 
-# Get the benchmark results for each hash functions.
-std_hash_results = np.zeros(N, dtype=float)
-clhash_results = np.zeros(N, dtype=float)
-boost_hash_results = np.zeros(N, dtype=float)
-for idx in range(1, N):
-    data_file = prefix + str(idx) + ".json"
-    # print("data_file: ", data_file)
-    data = json.load(open(data_file))
-    benchmark_results = data["benchmarks"]
-    # print(benchmark_results)
-
-    for info in benchmark_results:
-        benchmark_name = info["name"]
-        if (benchmark_name == "std_hash_string"):
-            std_hash_results[idx] = info["real_time"]
-        elif (benchmark_name == "clhash_string"):
-            clhash_results[idx] = info["real_time"]
-        elif (benchmark_name == "boost_hash_string"):
-            boost_hash_results[idx] = info["real_time"]
-        else:
-            print("How can we get here?")
-
-# Plot results
-x = np.arange(1, N + 1, 1)
-line1, line2, line3 = plt.plot(
-    x, std_hash_results, "r", x, clhash_results, "b", x, boost_hash_results, "o")
-plt.xlabel("String length (bytes)")
-plt.ylabel("Run time (ns)")
-# plt.legend("std::hash", "clhash", "boost::hash")
-plt.ylim(0, 200)
-plt.xlim(1, N)
-plt.grid()
-plt.legend((line1, line2, line3), ('std::hash', 'clhash', 'boost::hash'))
-plt.show()
+# Plot benchmark results
+plot_results(prefix, N)
