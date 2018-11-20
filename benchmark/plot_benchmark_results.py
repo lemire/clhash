228df478a84d07a9f3d47773b04aab36a6dbd300
Hung Dang <hdang@athenahealth.com>
2018-11-20 00:49:37 -0500

    Update plot script and add a picture which compares the performance of of hash functions using random strings.

--- benchmark/plot_benchmark_results.py
+++ benchmark/plot_benchmark_results.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 import json
 import numpy as np
 
-def plot_results(prefix, N):
+def plot_results(prefix, N, YLIM):
     # Get the benchmark results for each hash functions.
     std_hash_results = np.zeros(N, dtype=float)
     clhash_results = np.zeros(N, dtype=float)
     boost_hash_results = np.zeros(N, dtype=float)
+    xxhash_results = np.zeros(N, dtype=float)
+    farmhash_results = np.zeros(N, dtype=float)
     hybrid_hash_results = np.zeros(N, dtype=float)
     for idx in range(0, N):
         data_file = prefix + str(idx + 1) + ".json"
@@ -27,19 +29,24 @@ def plot_results(prefix, N):
                 boost_hash_results[idx] = info["real_time"]
             elif (benchmark_name == "hybrid_hash_string"):
                 hybrid_hash_results[idx] = info["real_time"]
+            elif (benchmark_name == "xxhash_string"):
+                xxhash_results[idx] = info["real_time"]
+            elif (benchmark_name == "farmhash_string"):
+                farmhash_results[idx] = info["real_time"]
             else:
                 print("How can we get here?")
 
     # Plot results
     x = np.arange(1, N + 1, 1)
-    line1, line2, line3, line4 = plt.plot(
-        x, std_hash_results, "r", x, clhash_results, "b", x, boost_hash_results, 'g', x, hybrid_hash_results, 'y')
+    lines = plt.plot(
+        x, std_hash_results, "r-+", x, clhash_results, "b-o", x, boost_hash_results, 'g-', x, hybrid_hash_results, 'y-^', x, xxhash_results, 'g-v', x, farmhash_results, 'm-*')
     plt.xlabel("String length (bytes)")
     plt.ylabel("Run time (ns)")
-    plt.ylim(0, 1000)
+    plt.ylim(0, YLIM)
     plt.xlim(1, N)
     plt.grid()
-    plt.legend((line1, line2, line3, line4), ('std::hash', 'clhash', 'boost::hash', 'hybrid_hash'))
+    plt.legend(lines, ('std::hash', 'clhash', 'boost::hash', 'hybrid_hash', 'xxHash', 'farmhash'))
+    plt.title("Hash functions performance analysis with random strings");
     plt.show()
 
 # Parse input arguments
@@ -48,13 +55,16 @@ parser.add_argument('prefix', metavar='prefix', type=str,
                     help='Prefix of JSON data files')
 parser.add_argument('maximum_length', metavar='maximum_length', type=int,
                     help='The maximum length of tested strings')
+parser.add_argument('ylim', metavar='ylim', type=int,
+                    help='The maximum length of tested strings')
 
 args = parser.parse_args()
 prefix = args.prefix
 N = args.maximum_length
+ylim = args.ylim
 print("Prefix: ", prefix)
 print("Maximum string length: ", N)
 N = args.maximum_length
 
 # Plot benchmark results
-plot_results(prefix, N)
+plot_results(prefix, N, ylim)
